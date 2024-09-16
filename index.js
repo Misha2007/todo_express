e4da69477269bb15b9e9ee4be7eeb8ba3d4f0c3d
Misha2007 <mishadrogovoz@gmail.com>
2024-09-16 09:30:10 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,20 +6,39 @@ const fs = require("fs");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./tasks", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const tasks = data.split("\n");
+      resolve(tasks);
+    });
+  });
+};
+
 app.get("/", (req, res) => {
-  fs.readFile("./tasks", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const tasks = data.split("\n");
+  readFile("./tasks").then((tasks) => {
     res.render("index", { tasks: tasks });
   });
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 app.post("/", (req, res) => {
-  form;
-  res.render("index");
+  readFile("./tasks").then((tasks) => {
+    tasks.push(req.body.task);
+    const data = tasks.join("\n");
+    fs.writeFile("./tasks", data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.redirect("/");
+    });
+  });
 });
 
 app.listen(3001, () => {
