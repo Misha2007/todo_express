a9c4031eebb37609bd1b7f687b5139a6d4ee731a
Misha2007 <mishadrogovoz@gmail.com>
2024-09-16 09:16:15 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const path = require("path")
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+const path = require("path");
+const fs = require("fs");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  fs.readFile("./tasks", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const tasks = data.split("\n");
+    res.render("index", { tasks: tasks });
+  });
+});
+
+app.post("/", (req, res) => {
+  form;
+  res.render("index");
+});
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3001")
-})
+  console.log("Server is running on port 3001");
+});
